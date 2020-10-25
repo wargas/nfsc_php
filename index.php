4128f3fcd14ff091cbbd78ddc3af6ecd1aecaa13
Wargas Teixeira <wargasteixeira@hotmail.com>
2020-10-25 00:55:47 -0300

    melhorias

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 require 'vendor/autoload.php';
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use \Slim\Http\Response;
+use \Slim\Http\Request;
 
 $app = new Slim\App;
 
