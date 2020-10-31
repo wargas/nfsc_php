d5c04e62b84499a61235a8e619365f5f24edec3d
Wargas Teixeira <wargasteixeira@hotmail.com>
2020-10-31 05:45:21 -0300

    teste

--- index.php
+++ index.php
@@ -18,36 +18,33 @@ $app->post('/', function(Request $request, Response $response) {
     $cadastro = new Nfsc\Cadastro($data);
     $item = new Nfsc\Item($data);
 
-    echo $mestre->getFileName();
-    echo file_put_contents("geradas/PE123456789123221U 2010N01M.001", $mestre->getTexto());
+    $mestre->generateFile();
+    $cadastro->generateFile();
+    $item->generateFile();
 
-    // $mestre->generateFile();
-    // $cadastro->generateFile();
-    // $item->generateFile();
+    $zip = new ZipArchive();
 
-    // $zip = new ZipArchive();
+    $zip->open("geradas/{$data->competencia}/{$data->competencia}.zip", ZipArchive::CREATE);
 
-    // $zip->open("geradas/{$data->competencia}/{$data->competencia}.zip", ZipArchive::CREATE);
+    $zip->addFile($mestre->getFileName());
+    $zip->addFile($cadastro->getFileName());
+    $zip->addFile($item->getFileName());
 
-    // $zip->addFile($mestre->getFileName());
-    // $zip->addFile($cadastro->getFileName());
-    // $zip->addFile($item->getFileName());
+    $zip->close();
 
-    // $zip->close();
-
-    // return $response->withJson([
-    //     "arquivos" => [
-    //         "mestre" => $mestre->getFileName(),
-    //         "cadastro" => $cadastro->getFileName(),
-    //         "item" => $item->getFileName(),
-    //         "zip" => "geradas/{$data->competencia}/{$data->competencia}.zip"
-    //     ],
-    //     "infos" => [
-    //         "mestre" => $mestre->info(),
-    //         "cadastro" => $cadastro->info(),
-    //         "item" => $item->info(),            
-    //     ]  
-    // ]);
+    return $response->withJson([
+        "arquivos" => [
+            "mestre" => $mestre->getFileName(),
+            "cadastro" => $cadastro->getFileName(),
+            "item" => $item->getFileName(),
+            "zip" => "geradas/{$data->competencia}/{$data->competencia}.zip"
+        ],
+        "infos" => [
+            "mestre" => $mestre->info(),
+            "cadastro" => $cadastro->info(),
+            "item" => $item->info(),            
+        ]  
+    ]);
 });
 
 $app->get('/', function(Request $request, Response $response) use($app) {
