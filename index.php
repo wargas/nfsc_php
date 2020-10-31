87ed4a64014a8677dc9176329ba713a848271b63
Wargas Teixeira <wargasteixeira@hotmail.com>
2020-10-31 05:31:36 -0300

    testando

--- index.php
+++ index.php
@@ -14,37 +14,37 @@ $app->post('/', function(Request $request, Response $response) {
 
     $data = json_decode(file_get_contents('php://input'));
     
-    $mestre = new Nfsc\Mestre($data);
-    $cadastro = new Nfsc\Cadastro($data);
-    $item = new Nfsc\Item($data);
+    // $mestre = new Nfsc\Mestre($data);
+    // $cadastro = new Nfsc\Cadastro($data);
+    // $item = new Nfsc\Item($data);
 
-    $mestre->generateFile();
-    $cadastro->generateFile();
-    $item->generateFile();
+    // $mestre->generateFile();
+    // $cadastro->generateFile();
+    // $item->generateFile();
 
-    $zip = new ZipArchive();
+    // $zip = new ZipArchive();
 
-    $zip->open("geradas/{$data->competencia}/{$data->competencia}.zip", ZipArchive::CREATE);
+    // $zip->open("geradas/{$data->competencia}/{$data->competencia}.zip", ZipArchive::CREATE);
 
-    $zip->addFile($mestre->getFileName());
-    $zip->addFile($cadastro->getFileName());
-    $zip->addFile($item->getFileName());
+    // $zip->addFile($mestre->getFileName());
+    // $zip->addFile($cadastro->getFileName());
+    // $zip->addFile($item->getFileName());
 
-    $zip->close();
+    // $zip->close();
 
-    return $response->withJson([
-        "arquivos" => [
-            "mestre" => $mestre->getFileName(),
-            "cadastro" => $cadastro->getFileName(),
-            "item" => $item->getFileName(),
-            "zip" => "geradas/{$data->competencia}/{$data->competencia}.zip"
-        ],
-        "infos" => [
-            "mestre" => $mestre->info(),
-            "cadastro" => $cadastro->info(),
-            "item" => $item->info(),            
-        ]  
-    ]);
+    // return $response->withJson([
+    //     "arquivos" => [
+    //         "mestre" => $mestre->getFileName(),
+    //         "cadastro" => $cadastro->getFileName(),
+    //         "item" => $item->getFileName(),
+    //         "zip" => "geradas/{$data->competencia}/{$data->competencia}.zip"
+    //     ],
+    //     "infos" => [
+    //         "mestre" => $mestre->info(),
+    //         "cadastro" => $cadastro->info(),
+    //         "item" => $item->info(),            
+    //     ]  
+    // ]);
 });
 
 $app->get('/', function(Request $request, Response $response) use($app) {
