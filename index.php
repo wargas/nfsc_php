9f9bd6f0629b9fda0c0601cde0508b55ef34b03d
Wargas Teixeira <wargasteixeira@hotmail.com>
2020-10-31 05:33:48 -0300

    teste

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $app->post('/', function(Request $request, Response $response) {
     $cadastro = new Nfsc\Cadastro($data);
     $item = new Nfsc\Item($data);
 
-    echo $mestre->getFileName();
+    echo $mestre->getTexto();
 
     // $mestre->generateFile();
     // $cadastro->generateFile();
